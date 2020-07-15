5a812599e1e7792de787226901f20dc29d8dbce6
joker-cat <jocker.ss.cat@gmail.com>
2020-07-15 14:32:59 +0800

    22

---
+++ index_js/container6_swiper.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 2,
+    spaceBetween: 110,
+    slidesPerGroup: 2,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    // pagination: {
+    //   el: '.swiper-pagination',
+    //   clickable: true,
+    // },
+    navigation: {
+      nextEl: '.swiper-button-next1',
+      prevEl: '.swiper-button-prev1',
+    },
+  });
+
+  
