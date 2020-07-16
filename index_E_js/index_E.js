f0635da4bbde0e6327fa21e795b19ccc96164b8f
joker-cat <jocker.ss.cat@gmail.com>
2020-07-16 13:25:30 +0800

    1

--- index_E_js/index_E.js
+++ index_E_js/index_E.js
@@ -1,12 +1,14 @@
 $('#burger').click(function () {
     $('#navbar').toggleClass('active');
 
-    if ($('#burger>img').attr('src') == './index_E_img/burger.png') {
+    if ($('#burger>img').attr('src') == './index_E_img/burger.svg') {
         $('#burger>img').attr('src', './index_E_img/櫻花-10.png');
         $('#menu_container').css('opacity', '1');
+        $('#backtogo').css('display','none');
 
     } else {
-        $('#burger>img').attr('src', './index_E_img/burger.png');
+        $('#burger>img').attr('src', './index_E_img/burger.svg');
         $('#menu_container').css('opacity', '0');
+        $('#backtogo').css('display','block');
     }
 }); 
